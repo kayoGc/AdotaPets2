b94157b760004ce516687e12f164372aa5a5f90c
kayoGc <kayivicti@gmail.com>
2023-05-18 20:16:58 -0300

    tela menu

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -2,13 +2,63 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnEntrar, btnSair;
+    TextInputEditText txtUsuario, txtSenha;
+    TextView lblCadastro;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtSenha = findViewById(R.id.txtSenha);
+        lblCadastro = findViewById(R.id.lblCadstro);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario, senha;
+
+                usuario = txtUsuario.getText().toString();
+                senha = txtSenha.getText().toString();
+
+                if (usuario.equals("etecia") && senha.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "Usuario ou senha invalidos", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+        lblCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CadusoActivity.class));
+            }
+        });
     }
+
 }
